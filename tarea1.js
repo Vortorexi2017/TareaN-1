e07175deaf3c77a174b1154389976eacf20e1857
Victor Montivero <victorabel46@gmail.com>
2020-07-16 23:02:52 -0300

    Parte A
    Complete la parte A dandole funcionalidad al boton calcular y al boton reset en el primer formulario.
    falta validar el formulario

--- tarea1.js
+++ tarea1.js
@@ -1,12 +1,6 @@
-// console.log('Hola papa');
+// Programar conducta del boton Calcular.
 
 document.querySelector('#calcular').onclick = function(event) {
-  const $añoActual = document.querySelector('#anio-actual');
-  const añoActual = Number($añoActual.value);
-  const $añoNacimiento = document.querySelector('#anio-nacimiento');
-  const añoNacimiento = Number($añoNacimiento.value);
-
-  //console.log(añoActual, añoNacimiento);
 
   mostrarEdad();
   calcularEdad();
@@ -17,5 +11,35 @@ function mostrarEdad(){
     let etiquetaEdad = document.querySelector('#etiqueta-edad');
     let inputEdad = document.querySelector('#input-edad');
 
-    console.log(etiquetaEdad, inputEdad);
-}    
+    etiquetaEdad.className = " ";
+    inputEdad.className = " ";
+}  
+
+ function calcularEdad(){
+    const $anioActual = document.querySelector('#anio-actual');
+    const anioActual = Number($anioActual.value);
+    const $anioNacimiento = document.querySelector('#anio-nacimiento');
+    const anioNacimiento = Number($anioNacimiento.value);
+  
+    const edad = (anioActual - anioNacimiento);
+    
+    const selector = document.querySelector('#input-edad');
+    selector.value = edad;
+    
+} 
+
+// Programar conducta del boton Resetear.
+
+document.querySelector('#resetear').onclick = function(event) {
+    
+    ocultarEdad();
+    event.preventDefault();
+}
+
+  function ocultarEdad(){
+    let etiquetaEdad = document.querySelector('#etiqueta-edad');
+    let inputEdad = document.querySelector('#input-edad');
+
+    etiquetaEdad.className = "oculto ";
+    inputEdad.className = "oculto";
+  }
