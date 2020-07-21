ab7d3e023a6e355e01ab0d2bf0ab4cedd4be2d8f
Victor Montivero <victorabel46@gmail.com>
2020-07-21 01:58:38 -0300

    Sigue Error
    Sigue el error pero tengo sueño..zzZzz

---
+++ test1.js
@@ -0,0 +1,6 @@
+function probarValidarAnioActual(){
+    console.assert(validarAnioActual(-5)=== "Año Actual debe ser mayor que cero", 
+    "validar nombre no valido que el valor no sea negativo");
+}
+
+probarValidarAnioActual();
